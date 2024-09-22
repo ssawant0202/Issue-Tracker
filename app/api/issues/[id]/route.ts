66c841823bd085a3a3c71a968e125e9dc1639ea0
Sidd <ssawant0202@gmail.com>
2024-09-21 18:56:17 -0700

    Built an API to delete issues 43.3

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -23,3 +23,17 @@ export async function PATCH(request: NextRequest, {params}:{params: {id:string}}
 
         return NextResponse.json(updatedIssue);
     }
+
+    export async function DELETE(request: NextRequest, {params}:{params: {id:string}}){
+        const issue = await prisma.issue.findUnique({
+            where:{id:parseInt(params.id)}
+        });
+
+        if(!issue)
+            return NextResponse.json({error:'Invalid issue'}, {status:404})
+        const deleteIssue = await prisma.issue.delete({
+            where:{id:issue.id}
+        }); 
+        return NextResponse.json(deleteIssue);
+    }
+
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -19,3 +19,4 @@ export async function POST(request: NextRequest){
 
     return NextResponse.json(newIssue, {status:201} );
 }
+
