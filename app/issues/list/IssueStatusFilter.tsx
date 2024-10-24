15849634a2874da1b7dd504ea5aadc13b1c2bb39
Sidd <ssawant0202@gmail.com>
2024-10-23 22:01:45 -0700

    Filter issues

---
+++ app/issues/list/IssueStatusFilter.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Select } from '@radix-ui/themes'
+import { Status } from '@prisma/client'
+import { useRouter } from 'next/navigation';
+
+const statuses : {label: string, value: Status}[] = [
+    {label: 'All', value : 'ALL'},
+    {label: 'Open', value: 'OPEN'},
+    {label: 'In Progress', value: 'IN_PROGRESS'},
+    {label: 'Closed', value: 'CLOSED'}
+]; 
+
+
+const IssueStatusFilter = () => {
+    const router = useRouter(); 
+  return (
+    <div>
+        <Select.Root onValueChange={(status) => {
+            if ( status === 'ALL')
+                router.push('/issues/list');
+            else{
+                const query = status? `?status=${status}`:'';
+                router.push('/issues/list' + query);    
+            }
+            
+        }}>
+            <Select.Trigger placeholder='Filter by status...'/>
+            <Select.Content>
+                {statuses.map(status => <Select.Item key = {status.value} value = {status.value}>
+                    {status.label}
+                </Select.Item>)}
+            </Select.Content>
+        </Select.Root>
+    </div>
+  )
+}
+
+export default IssueStatusFilter
---
+++ prisma/migrations/20241024022034_added_all_as_a_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `issue` MODIFY `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED', 'ALL') NOT NULL DEFAULT 'OPEN';
