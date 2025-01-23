affa673fa5197a3cd9504b7fa7f8ed58c7379b54
Sidd <ssawant0202@gmail.com>
2025-01-23 13:20:51 -0800

    Implemented Pagination UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
+import Pagination from "@/components/Pagination"
+
 
 export default function Home() {
   return (
-    <div>Dashboard</div>
+    <Pagination itemCount={100} pageSize={10} currentPage={10} />
   )
 }
