2efa7d20aab73f9525d9a31fb4235fe2e2999560
Sidd <ssawant0202@gmail.com>
2025-01-23 19:05:45 -0800

    Implemented backed pagination 2

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,9 +5,14 @@ import IssueActions from './IssueActions';
 import { Issue, Status } from '@prisma/client';
 import NextLink from 'next/link';
 import { ArrowUpIcon } from '@radix-ui/react-icons';
+import Pagination from '@/components/Pagination';
 
 interface Props{
-  searchParams: {status: Status, orderBy: keyof Issue};
+  searchParams: {
+    status: Status, 
+    orderBy: keyof Issue,
+    page: string;
+  };
 }
 
 const IssuesPage = async({searchParams}: Props) => {
@@ -26,12 +31,17 @@ const IssuesPage = async({searchParams}: Props) => {
   const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) 
   ? {[searchParams.orderBy]: 'asc'} : undefined; 
 
+  const page = parseInt(searchParams.page) || 1 ;
+  const pageSize = 10; 
   const issues = await prisma.issue.findMany({
     where:{
       status: status
     }, 
-    orderBy
+    orderBy,
+    skip: (page - 1 ) * pageSize, 
+    take: pageSize
   });
+  const issueCount = await  prisma.issue.count({where: {status}}) // { where : {status}} means filter by status
   return (
     <div>
       <IssueActions/> 
@@ -68,6 +78,11 @@ const IssuesPage = async({searchParams}: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination 
+        pageSize={pageSize} 
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   )
 }
