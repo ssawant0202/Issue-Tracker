bf3f87f09c859038e60253af7b4ea63414184b87
Sidd <ssawant0202@gmail.com>
2025-01-29 15:32:42 -0800

    Built Issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,27 @@ import NextLink from 'next/link';
 import { ArrowUpIcon } from '@radix-ui/react-icons';
 import Pagination from '@/components/Pagination';
 import LatestIssues from './LatestIssues';
+import IssueSummary from './IssueSummary';
 
 interface Props{
   page:number;
 }
-export default function Home({searchParams}: {searchParams: {page : string}}) {
+export default async function Home({searchParams}: {searchParams: {page : string}}) {
+  const open = await prisma.issue.count({
+    where: {status: 'OPEN'}
+  });
+
+  const closed = await prisma.issue.count({
+    where: {status: 'CLOSED'}
+  });
+  
+  const inProgress = await prisma.issue.count({
+    where: {status: 'IN_PROGRESS'}
+  });
+  
 
   return (
-    <LatestIssues/>
+    // <LatestIssues/>
+    <IssueSummary open = {open} inProgress={inProgress} closed={closed}/>
   )
 }
